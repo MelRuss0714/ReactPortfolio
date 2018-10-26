40bc1579c1d574465f25e7fb10abc00a2c95cdef
MelRuss0714 <melindasuerussell@yahoo.com>
2018-10-26 19:10:25 -0400

    ReactPorfolio

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Resume.css';
+import ResumePic1 from '../../assets/images/MelindaRussellResume1.png';
+import ResumePic2 from '../../assets/images/MelindaRussellResume2.png';
+import ResumeDOC from '../../assets/documents/MelindaRussellResume.docx';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 
@@ -13,7 +16,9 @@ export default class Resume extends React.Component {
         return (
             <div className='Resume'>
                 <Navbar />
-                <h1>Resume is Found Here</h1>
+                <a href={ResumeDOC} download><img className='Resume' src={ResumePic1} alt='Resume'/></a>
+                <a href={ResumeDOC} download><img className='Resume' src={ResumePic2} alt='Resume'/></a>
+
                 <Footer />
 
             </div>
