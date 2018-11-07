0e99311e15dfa8be7a9e507f3c73bedd3135b4f6
MelRuss0714 <melindasuerussell@yahoo.com>
2018-11-07 18:17:18 -0500

    Updates

---
+++ precache-manifest.67ab8a53eae785a89cd7ec33b0b10f39.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2b222d94a2c0148ef3e6899146cb715",
+    "url": "/ReactPortfolio/static/media/ProPicture.e2b222d9.jpg"
+  },
+  {
+    "revision": "9588d872557ddf85ba18",
+    "url": "/ReactPortfolio/static/css/main.069a0c47.chunk.css"
+  },
+  {
+    "revision": "b2394078c14213bf34f9",
+    "url": "/ReactPortfolio/static/js/1.b2394078.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "d0aa34c1a77b25b9a5afddb0fac708d9",
+    "url": "/ReactPortfolio/static/media/BubblinkPic.d0aa34c1.png"
+  },
+  {
+    "revision": "70262b52c83d8c223d1e061d50c7c809",
+    "url": "/ReactPortfolio/static/media/SmartRock.70262b52.png"
+  },
+  {
+    "revision": "730d2106dff8813508bf10de4ee167e1",
+    "url": "/ReactPortfolio/static/media/PoppyPic.730d2106.png"
+  },
+  {
+    "revision": "05b3be354073592452196aafbbb0ea76",
+    "url": "/ReactPortfolio/static/media/MelindaRussellResume.05b3be35.docx"
+  },
+  {
+    "revision": "9588d872557ddf85ba18",
+    "url": "/ReactPortfolio/static/js/main.9588d872.chunk.js"
+  },
+  {
+    "revision": "9265a0c32d882930861daac1dc20f39b",
+    "url": "/ReactPortfolio/static/media/crystalClick.9265a0c3.png"
+  },
+  {
+    "revision": "5e994a66759b4af6b20cb2ba649c73dc",
+    "url": "/ReactPortfolio/static/media/trivia.5e994a66.png"
+  },
+  {
+    "revision": "61e56ef39ba33abb6257440934f21141",
+    "url": "/ReactPortfolio/static/media/GifApi.61e56ef3.png"
+  },
+  {
+    "revision": "bc3a501d4db78c71fc95b3e117134875",
+    "url": "/ReactPortfolio/static/media/MelindaRussellResume1.bc3a501d.png"
+  },
+  {
+    "revision": "533be871f06a97cfcb59148a12e4ab35",
+    "url": "/ReactPortfolio/static/media/MelindaRussellResume2.533be871.png"
+  },
+  {
+    "revision": "70972f597fac80e1d1a36bd6f348d730",
+    "url": "/ReactPortfolio/static/media/MelindaRussellResume.70972f59.pdf"
+  },
+  {
+    "revision": "aef157e4485f3936e37f993988cf05c7",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
